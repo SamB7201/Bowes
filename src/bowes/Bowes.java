d348551e0e35c24be5130627022dd61119cda720
shb4@students.ptcollege.edu <>
2020-02-04 15:55:16 -0500

    this is my first commit

---
+++ src/bowes/Bowes.java
@@ -0,0 +1,12 @@
+
+package bowes;
+
+public class Bowes {
+
+    public static void main(String[] args) {
+        Bowes();
+    }
+    public static void Bowes(){
+        System.out.println("Samuel Bowes");
+    }
+}
